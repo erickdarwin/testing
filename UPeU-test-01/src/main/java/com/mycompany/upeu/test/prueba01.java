2ae554fb1f6ee4e9c76382c95efe6964f2262a66
Alumno <Alumno@DESKTOP-HR4LLEK>
2018-05-11 13:02:54 -0500

    3

--- UPeU-test-01/src/main/java/com/mycompany/upeu/test/prueba01.java
+++ UPeU-test-01/src/main/java/com/mycompany/upeu/test/prueba01.java
@@ -5,13 +5,25 @@
  */
 package com.mycompany.upeu.test;
 
+import com.sun.media.jfxmedia.logging.Logger;
+
 /**
  *
  * @author Alumno
  */
+
 public class prueba01 {
+    private static final prueba01 LOGGER = prueba01.getLogger( prueba01.class.getName() );
     
-    public static void main(String[] a){
-        System.out.println("Hola mundo");}
-    
+    public static void main(String[] args) {
+    LOGGER.info("Logging an INFO-level message");
+  }
+
+    private static prueba01 getLogger(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void info(String logging_an_INFOlevel_message) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
